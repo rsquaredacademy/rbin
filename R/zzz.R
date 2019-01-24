71532280844990087ab4aaa53913b81ebd715cfb
rsquaredin <rsquaredin@gmail.com>
2019-01-24 18:11:32 +0530

    option to update package

---
+++ R/zzz.R
@@ -0,0 +1,36 @@
+.onAttach <- function(...) {
+
+  if (!interactive() || stats::runif(1) > 0.1) return()
+
+  pkgs <- utils::available.packages()
+  
+  cran_version <- 
+    pkgs %>%
+    extract("rbin", "Version") %>%
+    package_version()
+
+  local_version <- utils::packageVersion("rbin")
+  behind_cran <- cran_version > local_version
+
+  tips <- c(
+    "Learn more about olsrr at https://github.com/rsquaredacademy/rbin/.",
+    "Use suppressPackageStartupMessages() to eliminate package startup messages.",
+    "Need help getting started with regression models? Visit: https://www.rsquaredacademy.com",
+    "Check out our interactive app for quick data exploration. Visit: https://apps.rsquaredacademy.com/."
+  )
+
+  tip <- sample(tips, 1)
+
+  if (interactive()) {
+    if (behind_cran) {
+      msg <- message("A new version of rbin is available with bug fixes and new features.")
+      message(msg, "\nWould you like to install it?")
+      if (utils::menu(c("Yes", "No")) == 1) {
+        utils::update.packages("rbin")
+      } 
+    } else {
+      packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+    }   
+  }   
+
+}
